f9c74610e1e6dc88bf7b2f5e597e29c744bada2d
michelle <foleymichelle9@gmail.com>
2020-07-19 14:03:51 -0500

    write test

--- spec/features/user_can_find_parks_spec.rb
+++ spec/features/user_can_find_parks_spec.rb
@@ -1,8 +1,30 @@
 require "rails_helper"
 
-RSpec.feature "As a user" do
-  it "When I select Tennessee from the form" do
+RSpec.describe"As a user" do
+  it "Shows national parks and their data" do
+    visit "/"
+    select "Tennesee", from: :state
+    click_on 'Find Parks'
 
+    expect(current_path).to eq(search_path)
+
+    expect(page).to have_content('There are 97 parks in Tennesee')
+    expect(page).to have_css('.park', count: 1)
+
+    within(first('.park')) do
+      within('.name') do
+          expect(page).to have_content('park')
+      end
+      within('.description') do
+          expect(page).to have_content('description')
+      end
+      within('.direction') do
+          expect(page).to have_content('Fire Nation')
+      end
+      within('.hours') do
+          expect(page).to have_content('Aang')
+      end
+    end
   end
 end
 
