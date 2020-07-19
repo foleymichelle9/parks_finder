f98bb084e5d10e633a8cd240f668a019888ddb87
michelle <foleymichelle9@gmail.com>
2020-07-18 23:55:44 -0500

    Set up Api key

---
+++ spec/features/user_can_find_parks_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.feature "As a user" do
+  it "When I select Tennessee from the form" do
+
+  end
+end
+
+
+
+
+
+
+# As a user,
+# When I select "Tennessee" from the form,
+# (Note: use the existing form)
+# And click on "Find Parks",
+# I see the total of parks found,
+# And for each park I see:
+# - full name of the park
+# - description
+# - direction info
+# - standard hours for operation
